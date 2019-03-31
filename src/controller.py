800b184703dfbbd893bb200ceff15de89c4bcb47
xenon <lgasyou@163.com>
2019-03-31 17:34:37 +0800

    Add type annotations for interface Communicator.

--- src/controller.py
+++ src/controller.py
@@ -72,7 +72,6 @@ class OptimizationController(object):
 
     # Pre-train DQN model with offline data
     def pre_train_model(self):
-        self.mem.save()
         # Try to load data from file, if fails run training set and
         # save them into memory.
         if not self.mem.try_load_from_file():
@@ -84,7 +83,7 @@ class OptimizationController(object):
                 for (state, action, reward, terminal) in step:
                     self.mem.append(state, action, reward, terminal)
 
-            # Save data as 'mem.pk'
+            # Save data as 'pre-train-replay-memory.pk'
             self.mem.save()
 
         # Pre-train DQN model by using training set
--- src/env/communicator.py
+++ src/env/communicator.py
@@ -1,23 +1,26 @@
+import torch
+
+
 class Communicator(object):
     """
     The interface of Communicator.
     """
 
     @staticmethod
-    def get_action_set():
+    def get_action_set() -> dict:
         pass
 
-    def get_state_tensor(self):
+    def get_state_tensor(self) -> torch.Tensor:
         pass
 
-    def act(self, action):
+    def act(self, action) -> float:
         pass
 
-    def reset(self):
+    def reset(self) -> None:
         pass
 
-    def is_done(self):
+    def is_done(self) -> bool:
         pass
 
-    def close(self):
+    def close(self) -> None:
         pass
