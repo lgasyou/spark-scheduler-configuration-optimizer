ebea19a0708e431a7654c49f90de901391e489a1
lgasyou <lgasyou@163.com>
2019-07-02 19:36:29 +0800

    Rewrite the test function.

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,9 @@
 import requests
+import json
 
 
 r = requests.get('http://localhost:18088/ws/v1/cluster/apps?states=RUNNING')
-print(r.text)
+d = eval(r.text.replace('false', 'False').replace('true', 'True'))
+with open('../results/dump.json', 'w') as f:
+    json.dump(d, f)
+    print(d)
